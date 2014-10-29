bfaeae94675dc43751fffaac91acb55ac2286dba
Lowell Bander <lowellbander@gmail.com>
2014-10-29 15:44:31 -0700

    violate.sql skeleton

---
+++ violate.sql
@@ -0,0 +1,9 @@
+/*Now From the constraints that you listed before, identify the ones that can be enforced using either primary key constraints, referential integrity (foreign key) constraints, or CHECK constraints and specify them in SQL. In order to get the full credit, you should specify at least
+
+Three primary key constraints
+Six referential integrity constraints, and
+Three CHECK constraints
+
+and express them in SQL.*/
+
+-- for each constraint, demonstrate at least one database modification that violates it, and explain why the operation violates the constraint. 
