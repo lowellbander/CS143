b0cb4279a2f496dea3e20dd6e07d076f55f9dd54
Lowell Bander <lowellbander@gmail.com>
2014-10-29 16:55:20 -0700

    skeleton

--- violate.sql
+++ violate.sql
@@ -1,9 +1,15 @@
-/*Now From the constraints that you listed before, identify the ones that can be enforced using either primary key constraints, referential integrity (foreign key) constraints, or CHECK constraints and specify them in SQL. In order to get the full credit, you should specify at least
-
-Three primary key constraints
-Six referential integrity constraints, and
-Three CHECK constraints
-
-and express them in SQL.*/
-
 -- for each constraint, demonstrate at least one database modification that violates it, and explain why the operation violates the constraint. 
+
+-- (PK1) every movie has a unique identification number
+-- (PK2) every actor has a unique identification number
+-- (PK3) every director has a unique primary key
+
+-- (CHECK1) no movies made before jesus was born
+-- (CHECK2) enforce the gender binary
+-- (CHECK3) you can't die before you're born
+
+-- (FK1) mid must reference an existing movie
+-- (FK2) mid must reference an existing movie
+-- (FK4) mid must reference an existing movie
+-- (FK5) aid must reference an existing actor
+-- (FK6) mid must reference an existing movie
