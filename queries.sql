31f11d2955e7add9fee20954dba0a7b2ffb4831c
cs143 <cs143@cs143.localdomain>
2014-10-29 02:19:31 -0700

    finished first query

--- queries.sql
+++ queries.sql
@@ -1,16 +1,22 @@
 --  Give me the names of all the actors in the movie 'Die Another Day'. Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).
 
-/*SELECT first, last
+SELECT DISTINCT first, last
+FROM Movie AS M, MovieActor AS MA, Actor AS A
+WHERE M.title = "Die Another Day" AND
+	M.id = MA.mid AND
+	MA.aid = A.id;
+
+-- In theory, this query should do the same, but takes too long.
+/*SELECT DISTINCT first, last
 FROM Actor
 WHERE id IN (
-);*/
-
 	SELECT aid
 	FROM MovieActor
 	WHERE mid = (
 		SELECT DISTINCT id
 		FROM Movie
-		WHERE title="Die Another Day");
+		WHERE title="Die Another Day")
+);*/
 
 -- Give me the count of all the actors who acted in multiple movies.
 
