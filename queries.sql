74964702598ef0b01cc3c06493729af8a11f33d4
cs143 <cs143@cs143.localdomain>
2014-10-29 02:06:24 -0700

    query returns aid's

--- queries.sql
+++ queries.sql
@@ -1,5 +1,17 @@
 --  Give me the names of all the actors in the movie 'Die Another Day'. Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).
 
+/*SELECT first, last
+FROM Actor
+WHERE id IN (
+);*/
+
+	SELECT aid
+	FROM MovieActor
+	WHERE mid = (
+		SELECT DISTINCT id
+		FROM Movie
+		WHERE title="Die Another Day");
+
 -- Give me the count of all the actors who acted in multiple movies.
 
 -- another query that you came up with
