f265cd40657dbfcdd847574e68a027f581d64faf
Lowell Bander <lowellbander@gmail.com>
2014-10-29 01:26:43 -0700

    added queries.sql skeleton

---
+++ queries.sql
@@ -0,0 +1,5 @@
+--  Give me the names of all the actors in the movie 'Die Another Day'. Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).
+
+-- Give me the count of all the actors who acted in multiple movies.
+
+-- another query that you came up with
