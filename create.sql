693d06d923908475f6af063d890ae07f94bfa3e5
Lowell Bander <lowellbander@gmail.com>
2014-10-29 03:38:12 -0700

    set engine to innodb

--- create.sql
+++ create.sql
@@ -3,7 +3,8 @@ CREATE TABLE Movie(
     title VARCHAR(100), 
     year INT, 
     rating VARCHAR(10), 
-    company VARCHAR(50));
+    company VARCHAR(50))
+    ENGINE = INNODB;
 
 CREATE TABLE Actor(
     id INT, 
@@ -11,39 +12,47 @@ CREATE TABLE Actor(
     first VARCHAR(20), 
     sex VARCHAR(6), 
     dob DATE, 
-    dod DATE);
+    dod DATE)
+    ENGINE = INNODB;
 
 CREATE TABLE Director(
     id INT, 
     last VARCHAR(20), 
     first VARCHAR(20), 
     dob DATE, 
-    dod DATE);
+    dod DATE)
+    ENGINE = INNODB;
 
 CREATE TABLE MovieGenre(
     mid INT, 
-    genre VARCHAR(20));
+    genre VARCHAR(20))
+    ENGINE = INNODB;
 
 CREATE TABLE MovieDirector(
     mid INT, 
-    did INT);
+    did INT)
+    ENGINE = INNODB;
 
 CREATE TABLE MovieActor(
     mid INT, 
     aid INT, 
-    role VARCHAR(50));
+    role VARCHAR(50))
+    ENGINE = INNODB;
 
 CREATE TABLE Review(
     name VARCHAR(20), 
     time TIMESTAMP, 
     mid INT, 
     rating INT, 
-    comment VARCHAR(500));
+    comment VARCHAR(500))
+    ENGINE = INNODB;
 
 CREATE TABLE MaxPersonID(
-    id INT);
+    id INT)
+    ENGINE = INNODB;
 
 CREATE TABLE MaxMovieID(
-    id INT);
+    id INT)
+    ENGINE = INNODB;
 
 
