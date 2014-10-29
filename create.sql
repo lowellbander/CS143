12003e3598b735b7a58dfaec686d546a8ce2a0ed
Lowell Bander <lowellbander@gmail.com>
2014-10-29 15:43:32 -0700

    changed primary constraint implementation

--- create.sql
+++ create.sql
@@ -1,29 +1,32 @@
 CREATE TABLE Movie(
-    id INT PRIMARY KEY, -- every movie has a unique identification number
+    id INT NOT NULL, -- every movie has a unique identification number
     title VARCHAR(100), 
     year INT, 
     rating VARCHAR(10), 
     company VARCHAR(50),
-    CHECK (year > 0)) -- no movies made before jesus was born
+    CHECK (year > 0), -- no movies made before jesus was born
+    PRIMARY KEY(id)) 
     ENGINE = INNODB;
 
 CREATE TABLE Actor(
-    id INT PRIMARY KEY, -- every actor has a unique identification number
+    id INT NOT NULL, -- every actor has a unique identification number
     last VARCHAR(20), 
     first VARCHAR(20), 
     sex VARCHAR(6), 
     dob DATE, 
     dod DATE, 
-    CHECK (sex="male" OR sex="female")) -- enforce the gender binary
+    CHECK (sex="male" OR sex="female"), -- enforce the gender binary
+    PRIMARY KEY(id)) 
     ENGINE = INNODB;
 
 CREATE TABLE Director(
-    id INT PRIMARY KEY, -- every director has a unique primary key
+    id INT NOT NULL, -- every director has a unique primary key
     last VARCHAR(20), 
     first VARCHAR(20), 
     dob DATE, 
     dod DATE,
-    CHECK (dod IS NULL OR (dod > dob))) -- you can't die before you're born
+    CHECK (dod IS NULL OR (dod > dob)), -- you can't die before you're born
+    PRIMARY KEY(id)) 
     ENGINE = INNODB;
 
 CREATE TABLE MovieGenre(
