b73900279d52a325a96305979233f14d978d6150
Lowell Bander <lowellbander@gmail.com>
2014-10-29 23:20:31 -0700

    works, needs headers

--- vm-shared/query.php
+++ vm-shared/query.php
@@ -23,19 +23,24 @@
                 print "Connection failed: $errmsg <br />";
                 exit(1);
             }
-		    mysql_select_db("TEST", $db_connection);
+		    mysql_select_db("CS143", $db_connection);
 
             $query = $_GET["query"];
             $sanitized_query = mysql_real_escape_string($query, $db_connection);
             $rs = mysql_query($sanitized_query, $db_connection);
+            $nFields = mysql_num_fields($rs);
 
             print "<b>YOUR QUERY: </b>$sanitized_query<br><br>";
+            print "<b>nFields: </b>$nFields<br><br>";
 
 		    while($row = mysql_fetch_row($rs)) {
-		    	$sid = $row[0];
-		    	$name = $row[1];
-		    	$email = $row[2];
-		    	print "$sid, $name, $email<br />";
+                $printme = "";
+                for ($i = 0; $i < $nFields; $i++) {
+                    $printme += $row[$i] + ", ";
+                    print $row[$i];
+                    print " ";
+                }
+                print "<br>";
 		    }
 
 		    // close the connection when done
