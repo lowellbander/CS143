c42e4e597d91c442e809e73bb7a027705377913a
Lowell Bander <lowellbander@gmail.com>
2014-10-29 23:07:15 -0700

    only queries when query given

--- vm-shared/query.php
+++ vm-shared/query.php
@@ -16,29 +16,31 @@
     <br>
 	
 	<?php
-		$db_connection = mysql_connect("localhost", "cs143", "");
-        if(!$db_connection) {
-            $errmsg = mysql_error($db_connection);
-            print "Connection failed: $errmsg <br />";
-            exit(1);
+        if ( !empty( $_GET['query'])) {
+		    $db_connection = mysql_connect("localhost", "cs143", "");
+            if(!$db_connection) {
+                $errmsg = mysql_error($db_connection);
+                print "Connection failed: $errmsg <br />";
+                exit(1);
+            }
+		    mysql_select_db("TEST", $db_connection);
+
+            $query = $_GET["query"];
+            $sanitized_query = mysql_real_escape_string($query, $db_connection);
+            $rs = mysql_query($sanitized_query, $db_connection);
+
+            print "<b>YOUR QUERY: </b>$sanitized_query<br><br>";
+
+		    while($row = mysql_fetch_row($rs)) {
+		    	$sid = $row[0];
+		    	$name = $row[1];
+		    	$email = $row[2];
+		    	print "$sid, $name, $email<br />";
+		    }
+
+		    // close the connection when done
+		    mysql_close($db_connection);
         }
-		mysql_select_db("TEST", $db_connection);
-
-        $query = $_GET["query"];
-        $sanitized_query = mysql_real_escape_string($query, $db_connection);
-        $rs = mysql_query($sanitized_query, $db_connection);
-
-        print "<b>YOUR QUERY: </b>$sanitized_query<br><br>";
-
-		while($row = mysql_fetch_row($rs)) {
-			$sid = $row[0];
-			$name = $row[1];
-			$email = $row[2];
-			print "$sid, $name, $email<br />";
-		}
-
-		// close the connection when done
-		mysql_close($db_connection);
 	?>
 
 
