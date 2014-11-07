94c99b06dde06381bba7dc8d8ff2f66033318e08
Lowell Bander <lowellbander@gmail.com>
2014-11-07 11:32:55 -0800

    added index.php skeleton

---
+++ vm-shared/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BBDB - Bakshi Bander Database</title>
+    </head>
+    <body>
+        <h1>BBDB - Bakshi Bander Database</h1>
+    </body>
+<html>
