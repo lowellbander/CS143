2ac84ed93ddb35a05aee585f3060da76a2f11d73
Lowell Bander <lowellbander@gmail.com>
2014-11-07 15:01:10 -0800

    framed

--- vm-shared/index.php
+++ vm-shared/index.php
@@ -2,8 +2,22 @@
 <html>
     <head>
         <title>BBDB - Bakshi Bander Database</title>
+        <style>
+            .nav {
+                float: left;
+                width:24%;
+            }
+            .content {
+                float: right;
+                width:74%;
+            }
+        </style>
     </head>
-    <body>
+    <body style="overflow:hidden">
         <h1>BBDB - Bakshi Bander Database</h1>
+        <div>
+            <iframe class="view nav" name="nav" src="nav.php"></iframe>
+            <iframe class="view content" name="content" src="content.php"></iframe>
+        </div>
     </body>
 <html>
