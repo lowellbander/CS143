b91f53e482cd6ce5270ea33b81efabe00248125a
Lowell Bander <lowellbander@gmail.com>
2014-11-09 14:40:55 -0800

    skeleton

---
+++ vm-shared/addActorDirector.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Add An Actor or Director</title>
+    </head>
+    <body>
+        <h1>Add An Actor or Director</h1>
+        <form>
+            Role: <input type="radio" name="role" value="actor">Actor
+<input type="radio" name="role" value="director">Director<br>
+            First Name: <input type="text"><br>
+            Last Name: <input type="text"><br>
+            Sex: <input type="radio" name="sex" value="male">Male
+<input type="radio" name="sex" value="female">Female<br>
+            Date of Birth: <input type="text"><br>
+            Date of Death: <input type="text"><br>
+            <input type="submit" value="Add Actor/Director">
+        </form>
+    </body>
+<html>
