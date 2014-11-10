8537279209835d566fe9a7c15e32b5c66eaf8fd1
Lowell Bander <lowellbander@gmail.com>
2014-11-09 23:34:34 -0800

    adds genre

--- vm-shared/addMovie.php
+++ vm-shared/addMovie.php
@@ -17,6 +17,26 @@
             <option value="R">R</option>
             <option value="NC-17">NC-17</option>
             </select>
+            Genre: <input type="checkbox" name="genre_Action" value="Action">Action</input>
+            <input type="checkbox" name="genre_Adult" value="Adult">Adult</input>
+            <input type="checkbox" name="genre_Adventure" value="Adventure">Adventure</input>
+            <input type="checkbox" name="genre_Animation" value="Animation">Animation</input>
+            <input type="checkbox" name="genre_Comedy" value="Comedy">Comedy</input>
+            <input type="checkbox" name="genre_Crime" value="Crime">Crime</input>
+            <input type="checkbox" name="genre_Documentary" value="Documentary">Documentary</input>
+            <input type="checkbox" name="genre_Drama" value="Drama">Drama</input>
+            <input type="checkbox" name="genre_Family" value="Family">Family</input>
+            <input type="checkbox" name="genre_Fantasy" value="Fantasy">Fantasy</input>
+            <input type="checkbox" name="genre_Horror" value="Horror">Horror</input>
+            <input type="checkbox" name="genre_Musical" value="Musical">Musical</input>
+            <input type="checkbox" name="genre_Mystery" value="Mystery">Mystery</input>
+            <input type="checkbox" name="genre_Romance" value="Romance">Romance</input>
+            <input type="checkbox" name="genre_Sci-Fi" value="Sci-Fi">Sci-Fi</input>
+            <input type="checkbox" name="genre_Short" value="Short">Short</input>
+            <input type="checkbox" name="genre_Thriller" value="Thriller">Thriller</input>
+            <input type="checkbox" name="genre_War" value="War">War</input>
+            <input type="checkbox" name="genre_Western" value="Western">Western</input>
+            <br>
             <input type="submit" value="Add Movie">
         </form>
     </body>
@@ -69,6 +89,20 @@
 
             } else {
                     print "<b>Sorry bro, bad SQL. Maybe invalid syntax, or the command violated a CHECK constraint. Bummer.</b>";
+            }
+
+
+            // update the MovieGenre table
+            $genres = array();
+            foreach ($_GET as $key => $value) {
+                if (strpos($key, 'genre_') !== false) {
+                    array_push($genres, $value);
+                }
+            }
+
+            foreach ($genres as $value) {
+                $query = "INSERT INTO MovieGenre VALUES ($newID, '$value');";
+                query($query);
             }
         }
     ?>
