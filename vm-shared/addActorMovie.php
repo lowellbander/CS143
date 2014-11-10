7993f02583f5457472e1d698c652dd5af8e26ce5
Lowell Bander <lowellbander@gmail.com>
2014-11-09 17:21:24 -0800

    adds actormovie relations

--- vm-shared/addActorMovie.php
+++ vm-shared/addActorMovie.php
@@ -5,19 +5,52 @@
     </head>
     <body>
         <h1>Add An Actor to a Movie</h1>
-        <form>
-            <input type="submit" value="Add">
-        </form>
     </body>
     <?php
         include 'exe_q.php';
         
+        print "<form action='./addActorMovie.php' method='GET'>";
+        // populate the dropdowns with data from the database
+
         $movies = query("SELECT id, title, year FROM Movie;");
-        print "<select name='mid'>";
+        print "Movie: <select name='mid'>";
         while ($row = mysql_fetch_array($movies)) {
             print "<option value='" . $row['id'] . "'>" . $row['title'] . " (" . $row['year'] . ") </option>";
         }
-        print "</select>";
+        print "</select><br>";
         
+        $actors = query("SELECT id, first, last, dob FROM Actor;");
+        print "Actor: <select name='aid'>";
+        while ($row = mysql_fetch_array($actors)) {
+            print "<option value='" . $row['id'] . "'>" . $row['first'] . " ". $row['last'] . " (" . $row['dob'] . ") </option>";
+        }
+        print "</select><br>";
+        print "Role: <input type='text' name='role'>";
+        print "<br><input type='submit' value='Add'></form>";
+
+        if (!empty($_GET['mid'])) {
+
+        // Build SQL query from URL params
+        
+        $role = $_GET['role'];
+
+        $query = "INSERT INTO MovieActor VALUES (" . $_GET['mid'] . "," . $_GET['aid'] . ",'" . $role . "');";
+            
+        //print "query: $query";
+
+        // execute query
+            
+       $rs = query($query);
+       
+        // echo success or failure
+    
+        if ($rs) {
+            print "<b>Successfully added Actor/Movie Relation!</b>";
+        } else {
+            print "<b>Sorry bro, bad SQL. Maybe invalid syntax, or the command violated a CHECK constraint. Bummer.</b>";
+        }
+
+        }
+
     ?>
 <html>
