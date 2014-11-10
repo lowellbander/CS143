778a11a0a8737e3b5efc34f6533602422bf64d68
Lowell Bander <lowellbander@gmail.com>
2014-11-09 19:43:47 -0800

    shows actor info, no movies stuff yet

--- vm-shared/movieInfo.php
+++ vm-shared/movieInfo.php
@@ -6,4 +6,40 @@
     <body>
         <h1>Movie Information</h1>
     </body>
+    <?php
+        include 'exe_q.php';
+
+
+        if (!empty($_GET['id'])) {
+            
+            // get information about the actor
+
+            $id = $_GET['id'];
+            $query = "SELECT * FROM Actor WHERE id = " . $id . ";";
+            $actor = query($query);
+
+            $name = "";
+            $sex = "";
+            $dob = "";
+            $dod = "";
+
+            while ($row = mysql_fetch_array($actor)) {
+                $name = $row['first'] . " " . $row['last'];
+                $sex = $row['sex'];
+                $dob = $row['dob'];
+                $dod = $row['dod'];
+            }
+
+            if ($dod == "") $dod = "~Still Living~";
+
+            // display information about the actor
+
+            print "Name: $name<br>";
+            print "Sex: $sex<br>";
+            print "Date of Birth: $dob<br>";
+            print "Date of Death: $dod<br>";
+
+        }
+
+    ?>
 <html>
